94592fc9a01b33614f0f19bbb38328aa011fdc96
Nikita Allaglo <nikita1999.allaglo@gmail.com>
2024-08-14 16:27:25 +0200

    Bad conflict solving after merge

--- src/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --job-name=test
-#SBATCH --mem=32GB
-#SBATCH --output=test.out
-#SBATCH --error=test.err
-
-./test.exe --input /home/allaglo/production/configs/T0.025_N10000/run_81.xy --outdir /home/allaglo/test/ --tau 100000 --lin 50 --log 50 --T 0.025 --N 10000
